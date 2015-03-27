b84e6dc67d292e3b3cb1bd4f186eb94e70bc8864
James Gubler <gubimail@gmail.com>
2015-03-27 14:07:49 -0600

    finish successful, failed, disputed scopes for lists and format view

--- source/james_charge_app/app/models/credit_card_charge.rb
+++ source/james_charge_app/app/models/credit_card_charge.rb
@@ -1,2 +1,10 @@
 class CreditCardCharge < ActiveRecord::Base
+  scope :successful, -> { where(paid: true, disputed: false) }
+  scope :failed, -> { where(paid: false) }
+  scope :disputed, -> { where(disputed: true) }
+
+  def getCustomerName
+    customer = Customer.find(self.customer_id)
+    return customer.first_name + " " + customer.last_name
+  end
 end
